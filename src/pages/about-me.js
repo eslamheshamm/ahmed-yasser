672312e69c4ecdfa17f92d1444563b71640b1f52
eslamheshamm <esl2mhesham@gmail.com>
2021-06-24 23:49:19 +0200

    building pages

---
+++ src/pages/about-me.js
@@ -0,0 +1,195 @@
+import React from "react";
+import { Link } from "gatsby";
+const FilledDot = () => (
+	<div className="w-4 mx-1 h-4 rounded-full bg-black"></div>
+);
+const EmptyDot = () => (
+	<div className="w-4 mx-1 h-4 rounded-full border-2 border-black"></div>
+);
+const AboutMe = () => {
+	return (
+		<main>
+			<section className="flex flex-col items-start">
+				<Link to="/" className="mx-12 mt-8 mb-4">
+					<svg
+						width="36"
+						height="59"
+						viewBox="0 0 36 59"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+							d="M27 57.5508C28.0588 58.5169 29.25 59 30.6728 59C33.6176 59 36 56.7134 36 53.8794C36 52.4623 35.3713 51.1419 34.2794 50.0791L12.4743 29.4678L34.2794 8.88865C35.4044 7.82587 36 6.50546 36 5.12063C36 2.28657 33.6176 2.34522e-06 30.6728 2.47395e-06C29.2169 2.53759e-06 28.0257 0.483081 27 1.44924L1.98529 25.2811C0.661763 26.5371 0.0330869 27.8897 -1.28949e-06 29.5C-1.22051e-06 31.0781 0.628675 32.3985 1.98529 33.7189L27 57.5508Z"
+							fill="black"
+						/>
+					</svg>
+				</Link>
+				<article className="py-8 pl-16 pr-28 w-6/12 shadow-md  m-12">
+					<h2 className="font-SFBold text-4xl mb-8">Development Skills</h2>
+					<ul className="font-SFRegular text-3xl">
+						<li className="flex justify-between items-center">
+							<p>UIKit</p>
+							<div className="flex ml-6">
+								<FilledDot />
+								<FilledDot />
+								<FilledDot />
+								<FilledDot />
+								<EmptyDot />
+							</div>
+						</li>
+						<li className="flex justify-between items-center">
+							<p>SwiftUI</p>
+							<div className="flex ml-6">
+								<FilledDot />
+								<FilledDot />
+								<FilledDot />
+								<FilledDot />
+								<EmptyDot />
+							</div>
+						</li>
+						<li className="flex justify-between items-center">
+							<p>CoreData</p>
+							<div className="flex ml-6">
+								<FilledDot />
+								<FilledDot />
+								<FilledDot />
+								<EmptyDot />
+								<EmptyDot />
+							</div>
+						</li>
+						<li className="flex justify-between items-center">
+							<p>Xcode</p>
+							<div className="flex ml-6">
+								<FilledDot />
+								<FilledDot />
+								<FilledDot />
+								<FilledDot />
+								<EmptyDot />
+							</div>
+						</li>
+						<li className="flex justify-between items-center">
+							<p>RxSwift</p>
+							<div className="flex ml-6">
+								<FilledDot />
+								<FilledDot />
+								<FilledDot />
+								<EmptyDot />
+								<EmptyDot />
+							</div>
+						</li>
+						<li className="flex justify-between items-center">
+							<p>FireBase</p>
+							<div className="flex ml-6">
+								<FilledDot />
+								<FilledDot />
+								<FilledDot />
+								<FilledDot />
+								<EmptyDot />
+							</div>
+						</li>
+						<li className="flex justify-between items-center">
+							<p>Version Control</p>
+							<div className="flex ml-6">
+								<FilledDot />
+								<FilledDot />
+								<FilledDot />
+								<FilledDot />
+								<EmptyDot />
+							</div>
+						</li>
+						<li className="flex justify-between items-center">
+							<p>CL/CI</p>
+							<div className="flex ml-6">
+								<FilledDot />
+								<FilledDot />
+								<FilledDot />
+								<EmptyDot />
+								<EmptyDot />
+							</div>
+						</li>
+					</ul>
+				</article>
+				<article className="py-8 pl-16 pr-28 w-6/12 shadow-md  m-12">
+					<h2 className="font-SFBold text-4xl mb-8">Development Skills</h2>
+					<ul className="font-SFRegular text-3xl">
+						<li className="flex justify-between items-center">
+							<p>Photoshop</p>
+							<div className="flex ml-6">
+								<FilledDot />
+								<FilledDot />
+								<FilledDot />
+								<FilledDot />
+								<EmptyDot />
+							</div>
+						</li>
+						<li className="flex justify-between items-center">
+							<p>Illustrator / Affinity Designer</p>
+							<div className="flex ml-6">
+								<FilledDot />
+								<FilledDot />
+								<FilledDot />
+								<FilledDot />
+								<FilledDot />
+							</div>
+						</li>
+						<li className="flex justify-between items-center">
+							<p>Figma / Sketch</p>
+							<div className="flex ml-6">
+								<FilledDot />
+								<FilledDot />
+								<FilledDot />
+								<EmptyDot />
+								<EmptyDot />
+							</div>
+						</li>
+						<li className="flex justify-between items-center">
+							<p>Origami Studio</p>
+							<div className="flex ml-6">
+								<FilledDot />
+								<FilledDot />
+								<FilledDot />
+								<EmptyDot />
+								<EmptyDot />
+							</div>
+						</li>
+					</ul>
+				</article>
+				<article className="py-8 pl-16 pr-28 w-6/12 shadow-md  m-12">
+					<h2 className="font-SFBold text-4xl mb-8">Soft Skills</h2>
+					<ul className="font-SFRegular text-3xl">
+						<li className="flex justify-between items-center">
+							<p>Critical thinking / Problem-solving</p>
+						</li>
+						<li className="flex justify-between items-center">
+							<p>Creativity</p>
+						</li>
+						<li className="flex justify-between items-center">
+							<p>Teamwork / Communication</p>
+						</li>
+						<li className="flex justify-between items-center">
+							<p>Organisation</p>
+						</li>
+					</ul>
+				</article>
+				<article className="py-8 pl-16 pr-28 w-6/12 shadow-md  m-12">
+					<h2 className="font-SFBold text-4xl mb-8">More About me</h2>
+					<ul className="font-SFRegular text-3xl">
+						<li className="flex justify-between items-center">
+							<p>Booksworm</p>
+						</li>
+						<li className="flex justify-between items-center">
+							<p>Runner</p>
+						</li>
+						<li className="flex justify-between items-center">
+							<p>Carpentery Lover</p>
+						</li>
+						<li className="flex justify-between items-center">
+							<p>Geek</p>
+						</li>
+					</ul>
+				</article>
+			</section>
+		</main>
+	);
+};
+export default AboutMe;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,184 +1,158 @@
-import * as React from "react"
+import * as React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+import { Link } from "gatsby";
+import AppPreview from "../components/app-preview";
+import soundfyLogo from "../images/soundfy.png";
+import soundfyScreen from "../images/soundfy-screen.png";
+import myTreeLogo from "../images/mytree.png";
+import myTreeScreen from "../images/mytree-screen.png";
+import astroLogo from "../images/astro.png";
+import astroScreen from "../images/astro-screen.png";
+const Apps = [
+	{
+		appName: "Soundfy",
+		appLogo: `${soundfyLogo}`,
+		appDescription:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tempor diam morbi est justo, vel dictum. Proin penatibus vitae ornare vehicula. Dignissim sed risus sagittis augue cum aliquet lacus. Egestas sit sed at vestibulum maecenas.",
+		appLink: "https://www.google.com/",
+		appScreen: `${soundfyScreen}`,
+	},
+	{
+		appName: "MyTree",
+		appLogo: `${myTreeLogo}`,
+		appDescription:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tempor diam morbi est justo, vel dictum. Proin penatibus vitae ornare vehicula. Dignissim sed risus sagittis augue cum aliquet lacus. Egestas sit sed at vestibulum maecenas.",
+		appLink: "https://www.google.com/",
+		appScreen: `${myTreeScreen}`,
+	},
+	{
+		appName: "Astro",
+		appLogo: `${astroLogo}`,
+		appDescription:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tempor diam morbi est justo, vel dictum. Proin penatibus vitae ornare vehicula. Dignissim sed risus sagittis augue cum aliquet lacus. Egestas sit sed at vestibulum maecenas.",
+		appLink: "https://www.google.com/",
+		appScreen: `${astroScreen}`,
+	},
+];
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
-
-// markup
 const IndexPage = () => {
-  return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
-  )
-}
+	return (
+		<main className="relative pb-72">
+			<section className=" min-h-screen">
+				<div className="flex justify-center my-8">
+					<figure>
+						<StaticImage
+							src="../images/hello.svg"
+							alt="hello"
+							placeholder="blurred"
+							layout="fixed"
+						/>
+					</figure>
+				</div>
+				<div className="flex justify-center">
+					<ul className="bg-[#212025] my-4 p-8 text-2xl md:px-10 md:py-[72px] flex justify-center  flex-col rounded-[44px] font-SFRegular  mx-auto tracking-wider">
+						<li className=" md:text-[45px] tracking-wider text-white md:leading-[54px]">
+							<span className="text-[#6E6D72] mr-3">1</span>
+							<span className="text-[#70BCC4]">myName</span>{" "}
+							<span className="text-[#D55E8C]">is</span> Ahmed,
+						</li>
+						<li className=" md:text-[45px] tracking-wider text-white flex md:leading-[54px] ">
+							<span className="text-[#6E6D72] mr-3">2</span>
+							<p>
+								I’m an iOS developer <br />
+								and illustrator from Egypt.
+							</p>
+						</li>
+					</ul>
+				</div>
+				<div className="flex justify-center flex-col">
+					<figure className="  mx-auto mt-12 animate-bounce">
+						<StaticImage
+							src="../images/mac.svg"
+							alt="mac"
+							placeholder="blurred"
+							layout="fixed"
+						/>
+					</figure>
+					<h2 className="flex justify-center ">
+						<Link to="/about-me" className="flex items-center">
+							<p className="font-SFRegular text-2xl md:text-[45px] mr-8  ">
+								More about me
+							</p>
+							<StaticImage
+								src="../images/right-arrow.svg"
+								alt="more about me"
+								placeholder="blurred"
+								layout="constrained"
+							/>
+						</Link>
+					</h2>
+					<h2 className="flex justify-center flex-col items-center mt-6">
+						<p className="font-SFRegular text-[45px] mb-2 ">
+							Awesome apps I made
+						</p>
+						<StaticImage
+							src="../images/down-arrow.svg"
+							alt="more about me"
+							placeholder="blurred"
+							layout="fixed"
+						/>
+					</h2>
+				</div>
+			</section>
+			{Apps.map((app, index) => {
+				return <AppPreview {...app} key={index} />;
+			})}
+			<footer className="absolute bottom-0  w-full">
+				<div className="flex flex-col text-center  justify-center mb-4 ">
+					<p className="font-SFRegular text-[45px] ">Get in touch</p>
+					<div className="flex justify-center my-6">
+						<a
+							href="http://google.com"
+							target="_blank"
+							rel="noopener noreferrer"
+							className="mr-16"
+						>
+							<StaticImage
+								src="../images/twitter-logo.svg"
+								alt="twitter"
+								placeholder="blurred"
+								layout="fixed"
+							/>
+						</a>
+						<a
+							href="http://google.com"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<StaticImage
+								src="../images/mail-logo.svg"
+								alt="twitter"
+								placeholder="blurred"
+								layout="fixed"
+							/>
+						</a>
+					</div>
+					<p className="font-SFRegular text-3xl flex items-center justify-center">
+						Made with
+						<span aria-label="img" className="text-[#EC5545]">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+								fill="currentColor"
+								className="mx-2"
+							>
+								<path d="M12 4.435c-1.989-5.399-12-4.597-12 3.568 0 4.068 3.06 9.481 12 14.997 8.94-5.516 12-10.929 12-14.997 0-8.118-10-8.999-12-3.568z" />
+							</svg>{" "}
+						</span>
+						by <span className="font-SFBold ml-2">me</span>
+					</p>
+				</div>
+			</footer>
+		</main>
+	);
+};
 
-export default IndexPage
+export default IndexPage;
